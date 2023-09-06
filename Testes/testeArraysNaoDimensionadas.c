6410e00a53c410c2f56a57d772178c20b9a90ca3
Lucian Bellini <lucianbellini1@gmail.com>
2023-09-06 20:19:02 -0300

    06-09

---
+++ Testes/testeArraysNaoDimensionadas.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int array[] = {0, 1, 2, 3, 4};
+    int contador = 0;
+
+    while (contador < 4)
+    {
+        printf("%d\n", array[contador]);
+        contador++;
+    }
+    
+
+    return 0;
+}
