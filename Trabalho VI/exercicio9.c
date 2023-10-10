01f02f7defde8815b4f7fad25bd87f679f243647
Lucian Bellini <lucianbellini1@gmail.com>
2023-10-10 20:33:49 -0300

    10-10

--- Trabalho VI/exercicio9.c
+++ Trabalho VI/exercicio9.c
@@ -2,3 +2,40 @@
 Infelizmente o algoritmo que calcula o valor do táxi tem as variáveis tempo e distância, ou seja, mesmo que o carro esteja parado em um sinal, a conta do taxi vai aumentando. Faça um programa que dado o tempo e a distância, calcule o valor a ser pago. O custo é 0,02 centavos por segundo e R$ 1,00 por quilômetro.
 Atenção: você já implementou esse programa antes, porém agora TODAS AS VARIÁVEIS devem ser manipuladas usando ponteiros
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    // Declaração das variárel
+    float tempo = 0;
+    float distancia = 0;
+    float valor = 0;
+    // Declartação dos ponteiros
+    float *ponteirotempo = &tempo;
+    float *ponteirodistancia = &distancia;
+    float *ponteirovalor = &valor;
+    // Demanda e salva as informações da aplicação
+    printf("Digite o tempo da viagem em segundos: \n");
+    scanf("%f", &tempo);
+    printf("Digite a distancia da viagem em km: \n");
+    scanf("%f", &distancia);
+
+    if (distancia < 1)
+    {
+        printf("Distancia menor que um kilometro nao sera calculada");
+        distancia = 0;
+    }
+
+    // Calcula manipulando os ponteiros
+    *ponteirotempo = (*ponteirotempo) * (0.02);
+    *ponteirodistancia = (*ponteirodistancia) * (1.00);
+    *ponteirovalor = *ponteirotempo + *ponteirodistancia;
+
+    // Imprime
+    printf("O valor que sera pago na viagem e de: %.2f\n", *ponteirovalor);
+
+    return 0;
+}
